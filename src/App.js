2c57aa8a605c4208d192f01a762ad72994284270
AndriiMoskalenko <andreymus0@gmail.com>
2022-05-05 11:31:06 +0200

    issueFix

--- src/App.js
+++ src/App.js
@@ -7,77 +7,83 @@ import "./Style/fonts.css"
 function App() {
   return (
     <div>
-      <div class="nav">
-        <div class="nav-left">
+      <div className="nav">
+        <div className="nav-left">
           <h2>Tasky</h2>
           <h2 id="arrow-text">&#8592;</h2>
         </div>
-        <div class="nav-right">
-          <div class="seacrh">
+        <div className="nav-right">
+          <div className="seacrh">
             <input
               id="search-tasks-text"
               type="text"
               placeholder="Search tasks..."
             />
           </div>
-          <div class="more">
-            <a id="text-nav-assignees">Assignees</a>
-            <a id="text-nav-show">Show</a>
-            <a id="tsext-nav-more">More</a>
+          <div className="more">
+            <a id="text-nav-assignees" href="/home">
+              Assignees
+            </a>
+            <a id="text-nav-show" href="/home">
+              Show
+            </a>
+            <a id="tsext-nav-more" href="/home">
+              More
+            </a>
           </div>
-          <div class="create">
+          <div className="create">
             <button id="button-nav-share"> Share</button>
             <button id="button-nav-create">+ Create New</button>
           </div>
         </div>
       </div>
-      <div class="main-app">
-        <div class="side-bar">
-          <div class="folder-side-bar">
+      <div className="main-app">
+        <div className="side-bar">
+          <div className="folder-side-bar">
             <span id="text-sidebar-folder">Folder</span>
             <span id="text-sidebar-arrow">&#8597;</span>
           </div>
-          <div class="search-side-bar-input">
+          <div className="search-side-bar-input">
             <input type="text" id="text-sidebar-search" placeholder="Search" />
           </div>
-          <div class="menu-side-bar">
+          <div className="menu-side-bar">
             <span id="text-sidebar-menu">MENU</span>
           </div>
-          <div class="propjects-side-bar">
+          <div className="propjects-side-bar">
             <span id="text-sidebar-dashboard">Dashboard</span>
             <span id="text-sidebar-projects">Projects</span>
             <span id="text-sidebar-tasks">Your tasks</span>
             <span id="text-sidebar-calendar">Calendar</span>
           </div>
-          <div class="general-side-bar">
+          <div className="general-side-bar">
             <span id="text-sidebar-general">General</span>
           </div>
-          <div class="support-side-bar">
+          <div className="support-side-bar">
             <span id="text-sidebar-settings">Settings</span>
             <span id="text-sidebar-support">Support</span>
           </div>
-          <div class="light-side-bar">
+          <div className="light-side-bar">
             <h2>Dark</h2>
             <h2>Lisht</h2>
           </div>
-          <div class="name-side-bar">
+          <div className="name-side-bar">
             <span id="text-sidebar-name">Name</span>
             <span id="text-sidebar-arrow-low">&#8595;</span>
           </div>
         </div>
-        <div class="center-main">
-          <div class="center-main-path">
+        <div className="center-main">
+          <div className="center-main-path">
             <p id="text-main-path-projects">PROJECTS</p>
             <p id="text-main-path-aro"></p>
             <p id="text-main-path-apps">APPS</p>
             <p id="text-main-path-arow"></p>
             <p id="text-main-path-moveapp">MOVE APP</p>
           </div>
-          <div class="center-main-name">
+          <div className="center-main-name">
             <span id="text-main-crypter">Crypter App</span>
             <span id="text-main-member">+ Member</span>
           </div>
-          <div class="center-main-tasks">
+          <div className="center-main-tasks">
             <div>
               <span id="text-main-path-tasks">Tasks</span>
               <span id="text-main-path-obj">Objectives</span>
@@ -89,18 +95,18 @@ function App() {
               <span id="text-main-path-code">&#x268F;</span>
             </div>
           </div>
-          <div class="center-main-lists">
-            <div class="todo">
-              <div class="todo-folder">
-                <h2 id="text-main-column" class="todos">
+          <div className="center-main-lists">
+            <div className="todo">
+              <div className="todo-folder">
+                <h2 id="text-main-column" className="todos">
                   TO DO
                 </h2>
                 <h2 id="text-main-plus">+</h2>
                 <h2 id="text-main-dots">...</h2>
               </div>
-              <div class="todo-blocks">
-                <div class="center-main-block">
-                  <div class="todo-research">
+              <div className="todo-blocks">
+                <div className="center-main-block">
+                  <div className="todo-research">
                     <span id="text-main-block-research">Research</span>
                     <span id="text-main-block-dots">...</span>
                   </div>
@@ -110,7 +116,7 @@ function App() {
                   <div>
                     <span id="text-main-block-low">Low</span>
                   </div>
-                  <div class="main-blocks-lower-num">
+                  <div className="main-blocks-lower-num">
                     <span id="text-main-block-number">3</span>
                     <span id="text-main-block-number">12</span>
                     <span id="text-main-block-number">1</span>
@@ -118,17 +124,17 @@ function App() {
                 </div>
               </div>
             </div>
-            <div class="todo">
-              <div class="todo-folder">
-                <h2 id="text-main-column" class="inwork">
+            <div className="todo">
+              <div className="todo-folder">
+                <h2 id="text-main-column" className="inwork">
                   IN WORK
                 </h2>
                 <h2 id="text-main-plus">+</h2>
                 <h2 id="text-main-dots-one">...</h2>
               </div>
-              <div class="todo-blocks">
-                <div class="center-main-block">
-                  <div class="todo-research">
+              <div className="todo-blocks">
+                <div className="center-main-block">
+                  <div className="todo-research">
                     <span id="text-main-block-research">Research</span>
                     <span id="text-main-block-dots">...</span>
                   </div>
@@ -138,7 +144,7 @@ function App() {
                   <div>
                     <span id="text-main-block-low">Low</span>
                   </div>
-                  <div class="main-blocks-lower-num">
+                  <div className="main-blocks-lower-num">
                     <span id="text-main-block-number">3</span>
                     <span id="text-main-block-number">12</span>
                     <span id="text-main-block-number">1</span>
@@ -146,17 +152,17 @@ function App() {
                 </div>
               </div>
             </div>
-            <div class="todo">
-              <div class="todo-folder">
-                <h2 id="text-main-column" class="review">
+            <div className="todo">
+              <div className="todo-folder">
+                <h2 id="text-main-column" className="review">
                   REVIEW
                 </h2>
                 <h2 id="text-main-plus">+</h2>
                 <h2 id="text-main-dots">...</h2>
               </div>
-              <div class="todo-blocks">
-                <div class="center-main-block">
-                  <div class="todo-research">
+              <div className="todo-blocks">
+                <div className="center-main-block">
+                  <div className="todo-research">
                     <span id="text-main-block-research">Research</span>
                     <span id="text-main-block-dots">...</span>
                   </div>
@@ -166,7 +172,7 @@ function App() {
                   <div>
                     <span id="text-main-block-low">Low</span>
                   </div>
-                  <div class="main-blocks-lower-num">
+                  <div className="main-blocks-lower-num">
                     <span id="text-main-block-number">3</span>
                     <span id="text-main-block-number">12</span>
                     <span id="text-main-block-number">1</span>
@@ -174,17 +180,17 @@ function App() {
                 </div>
               </div>
             </div>
-            <div class="todo">
-              <div class="todo-folder">
-                <h2 id="text-main-column" class="done">
+            <div className="todo">
+              <div className="todo-folder">
+                <h2 id="text-main-column" className="done">
                   DONE
                 </h2>
                 <h2 id="text-main-plus">+</h2>
                 <h2 id="text-main-dots">...</h2>
               </div>
-              <div class="todo-blocks">
-                <div class="center-main-block">
-                  <div class="todo-research">
+              <div className="todo-blocks">
+                <div className="center-main-block">
+                  <div className="todo-research">
                     <span id="text-main-block-research">Research</span>
                     <span id="text-main-block-dots">...</span>
                   </div>
@@ -194,7 +200,7 @@ function App() {
                   <div>
                     <span id="text-main-block-low">Low</span>
                   </div>
-                  <div class="main-blocks-lower-num">
+                  <div className="main-blocks-lower-num">
                     <span id="text-main-block-number">3</span>
                     <span id="text-main-block-number">12</span>
                     <span id="text-main-block-number">1</span>
