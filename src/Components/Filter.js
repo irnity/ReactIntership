61a8711d5dae44d627dda51915625c1c19427ee7
AndriiMoskalenko <andreymus0@gmail.com>
2022-05-13 19:24:50 +0300

    Filter

---
+++ src/Components/Filter.js
@@ -0,0 +1,103 @@
+import "../Style/filter.css"
+import { useState } from "react"
+
+const Filter = () => {
+  const data = [
+    { id: 1, name: "Igor", surname: "Ivanov", age: "27" },
+    { id: 2, name: "Gregory", surname: "Shabelnik", age: "44" },
+    { id: 3, name: "Danil", surname: "Ishytin", age: "19" },
+  ]
+  const [userList, setUserList] = useState(data)
+
+  const handleRemoveItem = (id) => {
+    setUserList(userList.filter((info) => info.id !== id))
+  }
+
+  const [enteredName, setEnteredName] = useState("")
+  const [enteredSurname, setEnteredSurname] = useState("")
+  const [enteredAge, setEnteredAge] = useState("")
+
+  const nameChangeHandler = (e) => {
+    setEnteredName(e.target.value)
+  }
+  const surnameChangeHandler = (e) => {
+    setEnteredSurname(e.target.value)
+  }
+  const ageChangeHandler = (e) => {
+    setEnteredAge(e.target.value)
+  }
+
+  const addUserHandler = (uName, uSurname, uAge) => {
+    setUserList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        {
+          name: uName,
+          surname: uSurname,
+          age: uAge,
+          id: Math.random().toString(),
+        },
+      ]
+    })
+  }
+
+  function addPersonHandler(e) {
+    e.preventDefault()
+    addUserHandler(enteredName, enteredSurname, enteredAge)
+    setEnteredName("")
+    setEnteredSurname("")
+    setEnteredAge("")
+    console.log(filterlist)
+  }
+
+  const filterlist = userList.map((user) => (
+    <div>
+      <li className="color" key={user.id}>
+        {user.name} {user.surname}, {user.age} year old,
+      </li>
+      <button className="button" onClick={() => handleRemoveItem(user.id)}>
+        delete
+      </button>
+    </div>
+  ))
+
+  return (
+    <div className="bg">
+      <div className="input card ">
+        <form className="" onSubmit={addPersonHandler}>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            onChange={nameChangeHandler}
+            value={enteredName}
+          />
+          <label htmlFor="surname">Surname</label>
+          <input
+            type="text"
+            id="surname"
+            onChange={surnameChangeHandler}
+            value={enteredSurname}
+          />
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            id="age"
+            onChange={ageChangeHandler}
+            value={enteredAge}
+          />
+          <button className="button" type="submit">
+            Add user
+          </button>
+        </form>
+      </div>
+      <div className="table">
+        <div className="cardt">
+          <div className="users ">{filterlist}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Filter
--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -1,10 +1,6 @@
 import React from "react"
 
 class ToDo extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     return (
       <div className="todo">
