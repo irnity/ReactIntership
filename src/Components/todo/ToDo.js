7844ba672e6a04bf858cc2baea5c125b89417250
AndriiMoskalenko <andreymus0@gmail.com>
2022-07-09 20:00:19 +0300

    cssImprove

--- src/Components/todo/ToDo.js
+++ src/Components/todo/ToDo.js
@@ -1,20 +1,47 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import ToDoView from './ToDoView';
 import ThemeContext from '../context/ThemeContext';
 
 function ToDo({
   name, firstValue, secondValue, thirdValue 
 }) {
   const theme = useContext(ThemeContext);
+
   return (
-    <ToDoView
-      theme={theme}
-      name={name}
-      firstValue={firstValue}
-      secondValue={secondValue}
-      thirdValue={thirdValue}
-    />
+    <div className="todo" style={theme}>
+      <div className="todo-folder-top">      
+        <div className="todo-folder">
+          <h2 id="text-main-column" className="inwork">
+            {name}
+          </h2>
+          <h2 id="text-main-plus">+</h2>
+          <h2 id="text-main-dots-one">...</h2>
+        </div>
+
+      </div>
+      <div className="todo-blocks-top">
+        <div className="todo-blocks">
+          <div className="center-main-block">
+            <div className="todo-research">
+              <span id="text-main-block-research">Research</span>
+              <span id="text-main-block-dots">...</span>
+            </div>
+            <div>
+              <span id="text-main-block-user">User Interview</span>
+            </div>
+            <div>
+              <span id="text-main-block-low">Low</span>
+            </div>
+            <div className="main-blocks-lower-num">
+              <span id="text-main-block-number">{firstValue}</span>
+              <span id="text-main-block-number">{secondValue}</span>
+              <span id="text-main-block-number">{thirdValue}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   );
 }
 
