1bbca4937dce60150ea76f4164b483bab6b62ccb
AndriiMoskalenko <andreymus0@gmail.com>
2022-07-13 11:29:14 +0300

    ApiUseEffect

--- src/Components/hero/Hero.js
+++ src/Components/hero/Hero.js
@@ -1,74 +1,55 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../Style/hero.css';
 
-class Hero extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      people: [],
-    };
-  }
-
-  componentDidMount() {
+function Hero() {
+  const [people, setPeople] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
     fetch('https://swapi.dev/api/people')
-      .then((response) => response.json())
-      .then(
-        (data) => {
-          this.setState({
-            isLoaded: true,
-            people: data.results,
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error,
-          });
-        }
-      );
-  }
-
-  render() {
-    const { error, isLoaded, people } = this.state;
-    if (error) {
-      return (
-        <p>
-          {' '}
-          Error
-          {error.message}
-        </p>
-      );
-    } if (!isLoaded) {
-      return <p> Loading...</p>;
-    } 
+      .then((res) => res.json())
+      .then((response) => { setPeople(response.results); setIsLoaded(true); })
+      .catch((err) => {
+        setError(err);
+      });
+  }, []);
+  if (error) {
     return (
-      <div className="hero-top">
-        <div className="hero-body">
-          <h1 className="color">Star Wars</h1>
-          <div className="hero">
-            {people.map((person) => (
-              <div key={people.name} className="hero-main">
-                <div className="hero-name">
-                  Name:
-                  {person.name}
-                </div>
-                <div className="hero-name">
-                  Height:
-                  {person.height}
-                </div>
-                <div className="hero-name">
-                  Mass:
-                  {person.mass}
-                </div>
+      <p>
+        Error:
+        {error.message}
+      </p>
+    );
+  } if (!isLoaded) {
+    return <p> Loading...</p>;
+  } 
+
+  return (
+    <div className="hero-top">
+      <div className="hero-body">
+        <h1 className="color">Star Wars</h1>
+        <div className="hero">
+          {error}
+          {people.map((person) => (
+            <div key={people.name} className="hero-main">
+              <div className="hero-name">
+                Name:
+                {person.name}
               </div>
-            ))}
-          </div>
+              <div className="hero-name">
+                Height:
+                {person.height}
+              </div>
+              <div className="hero-name">
+                Mass:
+                {person.mass}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Hero;
